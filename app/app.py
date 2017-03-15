d3b902e8aef5e140187731d3d9714ab777e0b894
justonlytest <imissmylovehah@163.com>
2017-03-15 22:13:41 +0800

    day6

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 #coding=utf-8
-from flask import Flask,render_template,redirect,request
+from flask import Flask,render_template,redirect,request,make_response,url_for,abort
 from werkzeug.routing import BaseConverter
+from werkzeug.utils import secure_filename
+from flask_script import Manager
+from os import path
 app = Flask(__name__)
-
+manager = Manager(app)
 class RegexConverter(BaseConverter):   #正则表达式类
     def __init__(self,url_map,*item):
         super(RegexConverter,self).__init__(url_map)
@@ -12,7 +15,10 @@ app.url_map.converters['regex'] = RegexConverter
 
 @app.route('/')
 def index():
-    return render_template('index.html',title="Welcome")
+    respone = make_response(render_template('index.html',title="Welcome"))
+    respone.set_cookie('username','this is onlytest')
+    return respone
+
 
 @app.route('/services')
 def services():
@@ -33,7 +39,34 @@ def user(user_id):
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+    else:
+        username = request.args['username']
     return render_template('login.html',method=request.method)
 
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        basepath = path.abspath(path.dirname(__file__))
+        upload_path = path.join(basepath,'static/upload')
+        f.save(upload_path,secure_filename(f.filename))
+        return redirect(url_for('upload'))
+    return render_template('upload.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'),404
+
+@manager.command
+def dev():
+    from livereload  import Server
+    live_server = Server(app.wsgi_app)
+    live_server.watch('**/*.*')
+    live_server.serve(open_url=True)
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
+    # app.run(debug=True)
