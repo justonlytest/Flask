e12a3e3e4ef4d42dd6d62951d6681fb464ec74c5
justonlytest <imissmylovehah@163.com>
2017-03-21 22:41:55 +0800

    工厂模式整改(day13)

--- app/Forms.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired
-class LoginForm(FlaskForm):
-    username = StringField(label=u'用户名',validators=[DataRequired()])
-    password = PasswordField(label=u'密码',validators=[DataRequired()])
-    submit = SubmitField(label=u'提交')
